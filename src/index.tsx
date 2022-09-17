4b977314e79eb37d563b106728f43a237e4c95c6
Tên của bạn <jungjihoonkr97@gmail.com>
2022-09-17 19:35:33 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
 import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('root')!;
